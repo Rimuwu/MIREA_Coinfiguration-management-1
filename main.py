2a086d21e1b6655734b0f07851a56f593d9ae6e0
AS1 <arseniykofanov@gmail.com>
2025-09-27 09:34:47 +0300

    Поддержка ввода аргументов при старте скрипта через argparse, поддержка запуска стартового скрипта

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import argparse
+import os
 from ShellEmulator import ShellEmulator
 
 
 def main():
-    emulator = ShellEmulator("VFS")
+    parser = argparse.ArgumentParser(
+        description="Эмулятор командной оболочки ")
+    parser.add_argument("--vfs-path",
+                        type=str, help="Путь к физическому расположению VFS")
+    parser.add_argument("--startup-script", 
+                        type=str, help="Путь к стартовому скрипту")
+
+    args = parser.parse_args()
+
+    vfs_name = os.path.basename(args.vfs_path) if args.vfs_path else "VFS"
+    emulator = ShellEmulator(vfs_name=vfs_name, 
+                             vfs_path=args.vfs_path, 
+                             startup_script=args.startup_script)
     emulator.run()
+
 
 if __name__ == "__main__":
     main()
